b5d565d674239c9d6a74160a401f4caefab3db47
nayan <nayankamalverma@gmail.com>
2024-07-06 11:08:15 +0530

    slicing a cell

--- Array-Minesweeper/header/Gameplay/Cell/CellModel.h
+++ Array-Minesweeper/header/Gameplay/Cell/CellModel.h
@@ -3,9 +3,43 @@ namespace Gameplay
 {
 	namespace Cell
 	{
+        enum class CellState
+        {
+            HIDDEN,
+            OPEN,
+            FLAGGED,
+        };
+
+        enum class CellValue
+        {
+            EMPTY,
+            ONE,
+            TWO,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            MINE,
+        };
+
 	   class CellModel
 	   {
-		   
+           CellState cell_state;
+           CellValue cell_value;
+
+       public:
+           CellModel();
+           ~CellModel();
+
+           CellState getCellState();
+           void setCellState(CellState state);
+
+           CellValue getCellValue();
+           void setCellValue(CellValue value);
+
+           void reset();
 	   };
 	}
 }
--- Array-Minesweeper/source/Gameplay/Cell/CellView.cpp
+++ Array-Minesweeper/source/Gameplay/Cell/CellView.cpp
@@ -1,5 +1,13 @@
 ﻿#include "../../header/Gameplay/Cell/CellView.h"
+
+#include <iostream>
+
+#include "../../header/Gameplay/Cell/CellModel.h"
+#include "../../header/Gameplay/Cell/CellController.h"
 #include "../../header/Global/Config.h"
+#include "../../header/Global/ServiceLocator.h"
+#include "../../header/Gameplay/GameplayService.h"
+#include "../../header/Sound/SoundService.h"
 
 namespace Gameplay
 {
@@ -18,12 +26,38 @@ namespace Gameplay
 
         void CellView::initialize()
         {
-            initializeButtonImage(tile_size * 3, tile_size * 3);
+            initializeButtonImage(tile_size*2 , tile_size*2 );
         }
 
         void CellView::initializeButtonImage(float width, float height)
         {
-            cell_button->initialize("Cell", Config::cells_texture_path, width, height, sf::Vector2f(0, 0));
+            cell_button->initialize("Cell", Config::cells_texture_path, width * slice_count, height, sf::Vector2f(0, 0));
+        }
+
+        void CellView::setCellTexture()
+        {
+            // Retrieves the cell's current value and converts it to an integer index
+            int index = static_cast<int>(cell_controller->getCellValue());
+
+            // Switch statement to handle different cell states 
+            switch (cell_controller->getCellState())
+            {
+            case::Gameplay::Cell::CellState::HIDDEN:
+                //If cell is hidden, then draw the 11th image, i.e. HIDDEN CELL
+                cell_button->setTextureRect(sf::IntRect(10 * tile_size, 0, tile_size, tile_size));
+                break;
+
+            case::Gameplay::Cell::CellState::OPEN:
+                //If cell is open, draw the image depending on its cell value
+                // 'index * tile_size' shifts the rectangle to display the texture corresponding to the cell's value
+                cell_button->setTextureRect(sf::IntRect(index * tile_size, 0, tile_size, tile_size));
+                break;
+
+            case::Gameplay::Cell::CellState::FLAGGED:
+                //If cell  is flagged, draw the 12th image, i.e. FLAG
+                cell_button->setTextureRect(sf::IntRect(11 * tile_size, 0, tile_size, tile_size));
+                break;
+            }
         }
 
         void CellView::update()
@@ -33,6 +67,7 @@ namespace Gameplay
 
         void CellView::render()
         {
+        	setCellTexture();
             cell_button->render();
         }
 	}
