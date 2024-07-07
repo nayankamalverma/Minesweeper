62b6cb7ce61709fc948e694496724c779dc569e1
nayan <nayankamalverma@gmail.com>
2024-07-07 21:38:24 +0530

    logic to handle player input

--- Array-Minesweeper/header/Gameplay/Cell/CellView.h
+++ Array-Minesweeper/header/Gameplay/Cell/CellView.h
@@ -5,25 +5,27 @@ namespace Gameplay
 {
 	namespace Cell
 	{
+        using namespace UI::UIElement;
         class CellController;
 
         class CellView
         {
         private:
 
-            const int tile_size = 32;
-            int slice_count = 12;
-
             const float cell_top_offset = 274.f;
             const float cell_left_offset = 583.f;
+
+            const int tile_size = 32;
+            const int slice_count = 12;
 
             UI::UIElement::ButtonView* cell_button;
             CellController* cell_controller;
 
-
             void setCellTexture();
             void initializeButtonImage(float width, float height);
             sf::Vector2f getCellScreenPosition(float width, float height);
+            void cellButtonCallback(ButtonType button_type);
+            void registerButtonCallback();
 
         public:
             CellView(CellController* controller);
